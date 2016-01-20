4ccd17919a2e0ed227cde8d7cdd0f143228408dc
ptaszysko <weronika.sawicka.9@gmail.com>
2016-01-20 01:57:51 +0100

    działa: wyświetlanie ostatnio wstawionych książek poprawić: edytowanie książek do zrobienia: koszyk, archiwum wypożyczeń, wyszukiwanie, archiwum wyszukiwania, obliczanie kary

--- src/java/Bean/BookBean.java
+++ src/java/Bean/BookBean.java
@@ -16,25 +16,111 @@ import javax.faces.model.ListDataModel;
 @ManagedBean
 @SessionScoped
 public class BookBean {
+
     private DataModel bookTitles;
+    private DataModel newBooks;
     private BookHelper helper;
-    
+    private int isbn;
+    private String author;
+    private String title;
+    private String description;
+    private String state;
+    private String category;
+    private boolean editing;
+
     public BookBean() {
         helper = new BookHelper();
+        isbn = 0;
+        author = null;
+        title = null;
+        description = null;
+        state = null;
+        category = null;
     }
-    
+
     public DataModel getBookTitles() {
         bookTitles = new ListDataModel(helper.getAll());
         return bookTitles;
     }
     
-    public void delete(String id) throws IOException{
+    public DataModel getLastThree(){
+        this.newBooks = new ListDataModel(helper.getLastThree());
+        return newBooks;
+    }
+
+    public void delete(String id) throws IOException {
         helper.delete(Integer.parseInt(id));
     }
-    
-    public void add(String isbn, String author, String title, String description, String state, String category) throws IOException{
-        Book book = new Book(Integer.parseInt(isbn), author, title, description, state, category);
-        helper.add(book);
-        FacesContext.getCurrentInstance().getExternalContext().redirect("admin/index.xhtml");
+
+    public void edit(String id) throws IOException {
+        Book b = helper.getById(Integer.parseInt(id));
+        this.author = b.getAuthor();
+        this.category = b.getCategory();
+        this.description = b.getDescription();
+        this.isbn = b.getIsbn();
+        this.state = b.getState();
+        this.title = b.getTitle();
+        this.editing = true;
+        FacesContext.getCurrentInstance().getExternalContext().redirect("addBook.xhtml");
     }
+
+    public void add(String isbn, String author, String title, String description, String state, String category) throws IOException {
+        Book book = new Book(Integer.parseInt(isbn), author, title, description, state, category);
+        if (editing == true) {
+            helper.edit(book);
+            editing = false;
+        } else {
+            helper.add(book);
+        }
+        FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
--- src/java/Helper/BookHelper.java
+++ src/java/Helper/BookHelper.java
@@ -13,33 +13,34 @@ import org.hibernate.Transaction;
  * @author mloda
  */
 public class BookHelper {
-    
-    public BookHelper(){}
-    
-    public List<Book> getAll(){
+
+    public BookHelper() {
+    }
+
+    public List<Book> getAll() {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         List<Book> list = new ArrayList<Book>();
-        try{
+        try {
             Transaction tx = session.beginTransaction();
-            Query q = session.createQuery ("from Book b");
+            Query q = session.createQuery("from Book b");
             list = (List<Book>) q.list();
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
             session.close();
         }
         return list;
     }
-    
-    public Book getById(int id){
+
+    public Book getById(int id) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         Book book = null;
-        try{
+        try {
             Transaction tx = session.beginTransaction();
-            Query q = session.createQuery ("from Book b where b.id = '" + id + "'");
-            book = (Book)q.uniqueResult();
+            Query q = session.createQuery("from Book b where b.id = '" + id + "'");
+            book = (Book) q.uniqueResult();
             tx = null;
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         } finally {
             session.close();
@@ -47,12 +48,30 @@ public class BookHelper {
         return book;
     }
     
-    public List<Book> getByNameAuthorISBN(String text){
+    public List<Book> getLastThree(){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        List<Book> books = null;
+        try {
+            Transaction tx = session.beginTransaction();
+            Query q = session.createQuery("select max(bb.id) from Book bb");
+            int s = (Integer) q.uniqueResult() - 3;
+            Query q2 = session.createQuery("from Book b where b.id >" + s);
+            books = (List<Book>) q2.list();
+            tx = null;
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            session.close();
+        }
+        return books;
+    }
+
+    public List<Book> getByNameAuthorISBN(String text) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         String[] split = text.split(" ");
         List<Book> books = new ArrayList<Book>();
-        for(int i=0;i>split.length;i++){
-            try{
+        for (int i = 0; i > split.length; i++) {
+            try {
                 Transaction tx = session.beginTransaction();
                 Query q = session.createQuery("from Book b where b.name = '" + split[i] + "'");
                 books.add((Book) q.list());
@@ -60,10 +79,10 @@ public class BookHelper {
             } catch (Exception ex) {
                 ex.printStackTrace();
             } finally {
-            session.close();
-        }
-            
-            try{
+                session.close();
+            }
+
+            try {
                 Transaction tx = session.beginTransaction();
                 Query q = session.createQuery("from Book b where b.author = '" + split[i] + "'");
                 books.add((Book) q.list());
@@ -71,10 +90,10 @@ public class BookHelper {
             } catch (Exception ex) {
                 ex.printStackTrace();
             } finally {
-            session.close();
-        }
-            
-            try{
+                session.close();
+            }
+
+            try {
                 Transaction tx = session.beginTransaction();
                 Query q = session.createQuery("from Book b where b.isbn = '" + split[i] + "'");
                 books.add((Book) q.list());
@@ -82,33 +101,71 @@ public class BookHelper {
             } catch (Exception ex) {
                 ex.printStackTrace();
             } finally {
-            session.close();
-        }
+                session.close();
+            }
         }
         return books;
     }
-    
-    public void add(Book book){
+
+    public void add(Book book) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         Transaction tx = session.beginTransaction();
-        try{
+        try {
             Query q = session.createQuery("from Book b where b.isbn='" + book.getIsbn() + "'");
-            if ((Book)q.uniqueResult() == null) session.save(book);
+            if ((Book) q.uniqueResult() == null) {
+                session.save(book);
+            }
             tx.commit();
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
-    
-    public void delete(int id){
+
+    public void edit(Book book) {
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
         Transaction tx = session.beginTransaction();
-        try{
+        try {
+            Query q = session.createQuery("from Book b where b.id='" + book.getId() + "'");
+            Book bookOld = (Book) q.uniqueResult();
+            if (bookOld != null) {
+                if (bookOld.getId().equals(book.getId()) != true) {
+                    bookOld.setId(book.getId());
+                }
+                if (bookOld.getIsbn() != (book.getIsbn())) {
+                    bookOld.setIsbn(book.getIsbn());
+                }
+                if (bookOld.getAuthor().equals(book.getAuthor()) != true) {
+                    bookOld.setAuthor(book.getAuthor());
+                }
+                if (bookOld.getTitle().equals(book.getTitle()) != true) {
+                    bookOld.setTitle(book.getTitle());
+                }
+                if (bookOld.getDescription().equals(book.getDescription()) != true) {
+                    bookOld.setDescription(book.getDescription());
+                }
+                if (bookOld.getState().equals(book.getState()) != true) {
+                    bookOld.setState(book.getState());
+                }
+                if (bookOld.getCategory().equals(book.getCategory()) != true) {
+                    bookOld.setCategory(book.getCategory());
+                }
+                session.update(bookOld);
+            }
+            tx.commit();
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void delete(int id) {
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        Transaction tx = session.beginTransaction();
+        try {
             Query q = session.createQuery("from Book b where b.id='" + id + "'");
-            Book book = (Book)q.uniqueResult();
+            Book book = (Book) q.uniqueResult();
             session.delete(book);
             tx.commit();
-        } catch(Exception ex){
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
